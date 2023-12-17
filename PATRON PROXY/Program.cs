164bed7223b228d4afd472bd080252590c967256
EmilyGallegos <153482337+EmilyGallegos@users.noreply.github.com>
2023-12-17 17:21:49 -0500

    Add files via upload

---
+++ PATRON PROXY/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(){
+        SeleccionCasas seleccionCasas = new SeleccionCasas ();
+
+        Estudiante estudiante1 = new EstudianteGryffindor {Nombre = "Hermione Granger", Edad = 12 , Materia favorita = Encantamientos};
+        Estudiante estudiante2 = new EstudianteHufflepuff {Nombre = "Cedric Diggory", Edad = 14 , Materia favorita = Posiones};
+        Estudiante estudiante3 = new EstudianteRavenclaw {Nombre = "Luna Lovegood", Edad = 11 , Materia favorita = Herbología};
+        Estudiante estudiante4 = new EstudianteSlytherin {Nombre = "Draco Malfoy", Edad = 13 , Materia favorita = Artes oscuras };
+
+        seleccionCasas.AsignarrCasa(estudiante1);
+        seleccionCasas.AsignarrCasa(estudiante2);
+        seleccionCasas.AsignarrCasa(estudiante3);
+        seleccionCasas.AsignarrCasa(estudiante4);
+
+        seleccionCasas.MostrarEstudianteAsignado();
+
+    }
+}
